5b8e3e2afe728257c5f837387bb0f0d980d8c77e
Florian <florian.bernou@gmail.com>
2017-01-18 10:30:45 +0100

    Creation Menu

---
+++ Can You Do It/Assets/Menu/Scripts/OnClick_Button.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function START_TUTO()
+{
+    Application.LoadLevel("Tuto/Level_001");
+}
+
+function START_GAME()
+{
+    Application.LoadLevel("Game/Level_001");
+}
+
+function LEAVE_GAME()
+{
+    Application.Quit();
+}
